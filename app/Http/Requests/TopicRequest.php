183993261d330e7b285beec8bb7bc3de9a126764
vagrant <vagrant@homestead>
2018-07-22 14:10:35 +0000

    User can create topic

--- app/Http/Requests/TopicRequest.php
+++ app/Http/Requests/TopicRequest.php
@@ -6,21 +6,44 @@ class TopicRequest extends Request
 {
     public function rules()
     {
-        switch($this->method())
+    //     switch($this->method())
+    //     {
+    //         // CREATE
+    //         case 'POST':
+    //         {
+    //             return [
+    //                 // CREATE ROLES
+    //             ];
+    //         }
+    //         // UPDATE
+    //         case 'PUT':
+    //         case 'PATCH':
+    //         {
+    //             return [
+    //                 // UPDATE ROLES
+    //             ];
+    //         }
+    //         case 'GET':
+    //         case 'DELETE':
+    //         default:
+    //         {
+    //             return [];
+    //         };
+    //     }
+    // }
+
+    switch($this->method())
         {
             // CREATE
             case 'POST':
-            {
-                return [
-                    // CREATE ROLES
-                ];
-            }
             // UPDATE
             case 'PUT':
             case 'PATCH':
             {
                 return [
-                    // UPDATE ROLES
+                    'title'       => 'required|min:2',
+                    'body'        => 'required|min:3',
+                    'category_id' => 'required|numeric',
                 ];
             }
             case 'GET':
@@ -36,6 +59,8 @@ class TopicRequest extends Request
     {
         return [
             // Validation messages
+            'title.min' => '标题必须至少两个字符',
+            'body.min' => '文章内容必须至少三个字符',
         ];
     }
 }
