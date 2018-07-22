4ce1db7356903855446d23ca76f388fdc2a0717e
vagrant <vagrant@homestead>
2018-07-22 17:51:01 +0000

    User can upload topic image

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
 use App\Models\Category;
+use App\Handles\ImageUploadHandler;
+use Auth;
 
 class TopicsController extends Controller
 {
@@ -50,7 +52,7 @@ class TopicsController extends Controller
         $topic->user_id = Auth::id();
         $topic->save();
 
-        return redirect()->route('topics.show', $topic->id)->with('message', 'Created Successfully');
+        return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
 	}
 
 	public function edit(Topic $topic)
@@ -73,5 +75,27 @@ class TopicsController extends Controller
 		$topic->delete();
 
 		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
-	}
+    }
+
+    public function uploadImage(Request $request, ImageUploadHandler $uploader)
+    {
+        // 初始化返回数据，默认是失败的
+        $data = [
+            'success'   => false,
+            'msg'       => '上传失败',
+            'file_path' => ''
+        ];
+        // 判断是否有文件上传，并赋值给 $file
+        if ($file = $request->upload_file) {
+            // 保存图片到本地
+            $result = $uploader->save($request->upload_file, 'topics', \Auth::id(), 1024);
+            // 图片保存成功的话
+            if ($result) {
+                $data['file_path'] = $result['path'];
+                $data['msg']       = '上传成功';
+                $data['success']   = true;
+            }
+        }
+        return $data;
+    }
 }
