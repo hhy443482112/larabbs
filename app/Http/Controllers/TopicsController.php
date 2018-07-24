e65a66108f01dfde08e4c63cc9c8250e7a0f5c66
vagrant <vagrant@homestead>
2018-07-24 14:43:47 +0000

    Auto translate slug

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -31,6 +31,11 @@ class TopicsController extends Controller
 
     public function show(Topic $topic)
     {
+        // URL矫正
+        if (! empty($topic->slug) && $topic->slug != $request->slug) {
+            return redirect($topic->link(), 301);
+        }
+
         return view('topics.show', compact('topic'));
     }
 
@@ -52,7 +57,8 @@ class TopicsController extends Controller
         $topic->user_id = Auth::id();
         $topic->save();
 
-        return redirect()->route('topics.show', $topic->id)->with('success', '成功创建话题');
+        // return redirect()->route('topics.show', $topic->id)->with('success', '成功创建话题');
+        return redirect()->to($topic->link())->with('success', '成功创建话题！');
 	}
 
 	public function edit(Topic $topic)
@@ -67,7 +73,8 @@ class TopicsController extends Controller
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('success', '更新成功');
+        // return redirect()->route('topics.show', $topic->id)->with('success', '更新成功');
+        return redirect()->to($topic->link())->with('success', '更新成功');
 	}
 
 	public function destroy(Topic $topic)
@@ -75,7 +82,8 @@ class TopicsController extends Controller
 		$this->authorize('destroy', $topic);
 		$topic->delete();
 
-		return redirect()->route('topics.index')->with('success', '删除成功');
+        // return redirect()->route('topics.index')->with('success', '删除成功');
+        return redirect()->to($topic->link())->with('success', '删除成功');
     }
 
     public function uploadImage(Request $request, ImageUploadHandler $uploader)
